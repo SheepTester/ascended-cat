e27d33be1661eee55cb0993ad88bd0248872fe57
SheepTester <ussa.say@gmail.com>
2019-11-22 19:06:30 -0800

    Move the events code into its own class so others can fire events too
    Newsletter is an unconventional name, but EventEmitter or whatever is 
    lacking in creativity

--- blocks/component.js
+++ blocks/component.js
@@ -1,9 +1,10 @@
 import {Elem} from '../utils/elem.js'
+import {Newsletter} from '../utils/elem.js'
 
-class GenericComponent {
+class GenericComponent extends Newsletter {
   constructor () {
+    super()
     this.measurements = null
-    this._listeners = {}
     this.position = {x: 0, y: 0}
   }
 
@@ -44,33 +45,6 @@ class GenericComponent {
       parent = parent.parent
     }
     return parent.workspace
-  }
-
-  on (eventName, fn) {
-    if (!this._listeners[eventName]) {
-      this._listeners[eventName] = []
-    }
-    this._listeners[eventName].push(fn)
-    return fn
-  }
-
-  off (eventName, fn) {
-    const listeners = this._listeners[eventName]
-    if (listeners) {
-      const index = listeners.indexOf(fn)
-      if (~index) {
-        listeners.splice(index, 1)
-      }
-    }
-  }
-
-  trigger (eventName, ...args) {
-    const listeners = this._listeners[eventName]
-    if (listeners) {
-      for (const fn of listeners) {
-        fn(...args)
-      }
-    }
   }
 }
 
---
+++ utils/newsletter.js
@@ -0,0 +1,37 @@
+/**
+ * A Newsletter allows one to subscribe to events that it may trigger.
+ */
+class Newsletter {
+  constructor () {
+    this._listeners = {}
+  }
+
+  on (eventName, fn) {
+    if (!this._listeners[eventName]) {
+      this._listeners[eventName] = []
+    }
+    this._listeners[eventName].push(fn)
+    return fn
+  }
+
+  off (eventName, fn) {
+    const listeners = this._listeners[eventName]
+    if (listeners) {
+      const index = listeners.indexOf(fn)
+      if (~index) {
+        listeners.splice(index, 1)
+      }
+    }
+  }
+
+  trigger (eventName, ...args) {
+    const listeners = this._listeners[eventName]
+    if (listeners) {
+      for (const fn of listeners) {
+        fn(...args)
+      }
+    }
+  }
+}
+
+export {Newsletter}
